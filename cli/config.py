b8e2696507b93c462ea83323ba4affb6f7f43fd2
Christian Svensson <blue@cmd.nu>
2022-01-09 11:40:03 +0100

    feat(cli): Add config command
    Writes settings (unused for now) for port and VRF.
    
    Signed-off-by: Christian Svensson <blue@cmd.nu>

---
+++ cli/config.py
@@ -0,0 +1,31 @@
+import click
+import utilities_common.cli as clicommon
+
+
+@click.group()
+def sonic_exporter():
+    """Configure Prometheus exporter for SONiC"""
+    pass
+
+
+@sonic_exporter.command('port')
+@click.argument('port', required=True, type=int)
+@clicommon.pass_db
+def set_port(db, port):
+    """Set the port that the exporter is listening to."""
+    db.cfgdb.mod_entry('SONIC_EXPORTER', 'default', {'port': port})
+
+
+@sonic_exporter.command('vrf')
+@click.argument('vrf', required=True)
+@clicommon.pass_db
+def set_vrf(db, vrf):
+    """Set the VRF that the exporter is listening inside."""
+    ctx = click.get_current_context()
+    if vrf not in db.cfgdb.get_table('VRF'):
+        ctx.fail('VRF {} does not exist'.format(vrf))
+    db.cfgdb.mod_entry('SONIC_EXPORTER', 'default', {'vrf': vrf})
+
+
+def register(cli):
+    cli.add_command(sonic_exporter)
